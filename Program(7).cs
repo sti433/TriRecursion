5fbd8a3673459000175ae553ead8110365e87d06
Setareh <sti.ri433@gmail.com>
2025-12-06 09:42:20 +0330

    Add files via upload

---
+++ Program(7).cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication144
+{
+    class Program
+    {
+        static void ShowMultiples(int n)
+        {
+            if (n < 3) return;
+
+            ShowMultiples(n - 3);
+            if ((n - (n % 3)) != 0)
+                Console.Write((n - (n % 3)) + " ");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("add: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("mezrab:");
+            ShowMultiples(n);
+            Console.ReadKey();
+        }
+    }
+}
